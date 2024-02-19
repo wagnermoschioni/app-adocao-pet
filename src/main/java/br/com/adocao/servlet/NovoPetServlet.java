0fbb109e6d0f2d36c0355775f0994ae4f733c883
Jose Wagner Fernandes Moschioni <wagner.moschioni@gmail.com>
2024-02-18 23:45:57 -0300

    novo form para cadastrar pet enviando parametros para servlet

--- src/main/java/br/com/adocao/servlet/NovoPetServlet.java
+++ src/main/java/br/com/adocao/servlet/NovoPetServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class NovoPetServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
 		String nomePet = request.getParameter("nome");
